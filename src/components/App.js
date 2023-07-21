bdfa9787a3c1be2061aef668e342fae95a355622
20Frederic20 <fredericpierrothountinme@gmail.com>
2023-07-21 18:00:09 +0100

    rewrite root folder to components

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import Banner from './Banner'
+import '../App.css';
+import ShoppingList from './ShoppingList';
+
+function App() {
+  return <div>
+    <Banner />
+    <ShoppingList />
+  </div>
+}
+
+export default App;
