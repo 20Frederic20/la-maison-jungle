064de8e3ec1f4bca20619992c7d12a21655a9292
20Frederic20 <fredericpierrothountinme@gmail.com>
2023-07-21 18:01:33 +0100

    assets and datats files

---
+++ src/datas/plantList.js
@@ -0,0 +1,80 @@
+export const plantList = [
+	{
+		name: 'monstera',
+		category: 'classique',
+		id: '1ed',
+        isBestSale: false,
+        isSpecialOffer: true,
+        water: 2,
+        light: 2,
+
+	},
+	{
+		name: 'ficus lyrata',
+		category: 'classique',
+		id: '2ab',
+        isBestSale: true,
+        water: 2,
+        light: 2,
+	},
+	{
+		name: 'pothos argenté',
+		category: 'classique',
+		id: '3sd',
+        isBestSale: false,
+        light: 2,
+        water: 2,
+	},
+	{
+		name: 'yucca',
+		category: 'classique',
+		id: '4kk',
+        isBestSale: true,
+        isSpecialOffer: true,
+        light: 2,
+        water: 2,
+
+	},
+	{
+		name: 'olivier',
+		category: 'extérieur',
+		id: '5pl',
+        isBestSale: false,
+        water: 2,
+        light: 2,
+	},
+	{
+		name: 'géranium',
+		category: 'extérieur',
+		id: '6uo',
+        isBestSale: true,
+        water: 2,
+        light: 2,
+	},
+	{
+		name: 'basilique',
+		category: 'extérieur',
+		id: '7ie',
+        isBestSale: false,
+        isSpecialOffer: true,
+        water: 2,
+        light: 2,
+
+	},
+	{
+		name: 'aloe',
+		category: 'plante grasse',
+		id: '8fp',
+        isBestSale: true,
+        water: 2,
+        light: 2,
+	},
+	{
+		name: 'succulente',
+		category: 'plante grasse',
+		id: '9vn',
+        isBestSale: false,
+        light: 2,
+        water: 2,
+	}
+]
