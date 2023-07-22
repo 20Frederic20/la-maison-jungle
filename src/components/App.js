31d634dfd4683db610d0dc88177d29d87cec4e09
20Frederic20 <fredericpierrothountinme@gmail.com>
2023-07-22 10:19:38 +0100

    use props children

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,17 @@
 import Banner from './Banner'
 import '../App.css';
 import ShoppingList from './ShoppingList';
+import logo from '../assets/logo.png'
+import Footer from './Footer';
 
 function App() {
   return <div>
-    <Banner />
+    <Banner>
+      <img src={logo} alt='La maison jungle' className='lmj-logo' />
+      <h1 className='lmj-title'>La maison jungle</h1>
+    </Banner>
     <ShoppingList />
+    <Footer />
   </div>
 }
 
