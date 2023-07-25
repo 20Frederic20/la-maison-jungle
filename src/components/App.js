8640dd7e8076db64b01512bfd819cfe7d7e873d4
20Frederic20 <fredericpierrothountinme@gmail.com>
2023-07-25 10:19:04 +0100

    Fin du projet la maisono jungle

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,27 @@ import '../App.css';
 import ShoppingList from './ShoppingList';
 import logo from '../assets/logo.png'
 import Footer from './Footer';
+import Cart from './Cart';
+import '../styles/Layout.css'
+import { useState } from 'react'
+
 
 function App() {
-  return <div>
-    <Banner>
-      <img src={logo} alt='La maison jungle' className='lmj-logo' />
-      <h1 className='lmj-title'>La maison jungle</h1>
-    </Banner>
-    <ShoppingList />
-    <Footer />
-  </div>
+  const [cart, updateCart] = useState([])
+  
+  return (
+      <div>
+          <Banner>
+              <img src={logo} alt='La maison jungle' className='lmj-logo' />
+              <h1 className='lmj-title'>La maison jungle</h1>
+          </Banner>
+          <div className='lmj-layout-inner'>
+              <Cart cart={cart} updateCart={updateCart} />
+              <ShoppingList cart={cart} updateCart={updateCart} />
+          </div>
+          <Footer />
+      </div>
+  )
 }
 
-export default App;
+export default App
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,42 +1,52 @@
-import { plantList } from "../datas/plantList"
+import { plantList } from '../datas/plantList'
+import PlantItem from './PlantItem'
 import '../styles/ShoppingList.css'
-import '../styles/plantItem.css'
-import CareScale from '../components/CareScale'
-import cover from '../assets/monstera.jpg'
 
-function ShoppingList() {
-    const categories = plantList.reduce(
+function ShoppingList({ cart, updateCart }) {
+	const categories = plantList.reduce(
 		(acc, plant) =>
 			acc.includes(plant.category) ? acc : acc.concat(plant.category),
 		[]
 	)
 
-    return (
-        <div>
-            <ul>
+	function addToCart(name, price) {
+		const currentPlantSaved = cart.find((plant) => plant.name === name)
+		if (currentPlantSaved) {
+			const cartFilteredCurrentPlant = cart.filter(
+				(plant) => plant.name !== name
+			)
+			updateCart([
+				...cartFilteredCurrentPlant,
+				{ name, price, amount: currentPlantSaved.amount + 1 }
+			])
+		} else {
+			updateCart([...cart, { name, price, amount: 1 }])
+		}
+	}
+
+	return (
+		<div className='lmj-shopping-list'>
+			<ul>
 				{categories.map((cat) => (
 					<li key={cat}>{cat}</li>
 				))}
-            </ul>
-            <ul className='lmj-plant-list'>
-				{plantList.map((plant) => (
-					<li key={plant.id} className='lmj-plant-item'  onClick={() => handleClick(plant.name)}>
-                        {/* <img className='lmj-plant-item-cover' src={cover} alt={`${plant.name} cover`} /> */}
-						{plant.name}
-						{plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
-                        <div>
-                            <CareScale careType='water' scaleValue={plant.water} />
-                            <CareScale careType='light' scaleValue={plant.light} />
-                        </div>
-					</li>
+			</ul>
+			<ul className='lmj-plant-list'>
+				{plantList.map(({ id, cover, name, water, light, price }) => (
+					<div key={id}>
+						<PlantItem
+							cover={cover}
+							name={name}
+							water={water}
+							light={light}
+							price={price}
+						/>
+						<button onClick={() => addToCart(name, price)}>Ajouter</button>
+					</div>
 				))}
 			</ul>
-        </div>
-    )
-}
-
-function handleClick(plantName) {
-    alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix 🌱✨`)
+		</div>
+	)
 }
 
 export default ShoppingList
